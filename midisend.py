04d885c659ce1128dbd461f7eda40bb29d74809f
georgekrueger <georgekrueger@gmail.com>
2022-01-08 11:55:48 -0600

    more work

--- midisend.py
+++ midisend.py
@@ -8,10 +8,10 @@ from datetime import datetime
 
 major_scale = [2, 2, 1, 2, 2, 2, 1]
 
-def generate_scale(scale, start_octave, length):
+def generate_scale(intervals, start_octave, length):
     notes = [start_octave * 12 + 12]
     while len(notes) < length:
-        interval = scale[(len(notes) - 1) % len(scale)]
+        interval = intervals[(len(notes) - 1) % len(intervals)]
         notes.append(notes[-1] + interval)
     return notes
 
@@ -31,38 +31,39 @@ class Track:
     def __init__(self, channel=0):
         self.channel = channel
         self.notes = []
-        self.cursor = 0
+        self.write_cursor = 0
+        self.play_cursor = 0
 
     def add_note(self, length, note_num=None, velocity=100, sustain=0):
         print("add note %s %s %s" % (length, note_num, velocity))
-        self.notes.append(asyncio.create_task(note(self.cursor, length + sustain, note_num, velocity, self.channel)))
-        self.cursor += length
+        self.notes.append((length + sustain, note_num, velocity))
+        self.write_cursor += length
 
     def add_rest(self, length):
         print("add rest %s" % length)
-        self.cursor += length
+        self.write_cursor += length
 
     async def play(self, loop=False):
-        # asyncio.get_event_loop().run_until_complete(asyncio.wait(self.notes))
-        done, pending = await asyncio.wait(self.notes)
-        while loop:
-            done, pending = await asyncio.wait(self.notes)
+        while 1:
+            n = self.notes[self.play_cursor]
+            await play_note(0, n[0], n[1], n[2])
+            self.play_cursor = (self.play_cursor + 1) % len(self.notes)
 
 async def main():
     track1 = Track()
-    notes = generate_scale(major_scale, 3, 21)
-    note = notes[0]
-    track1.add_note(1, note)
-    # for i in range(1, 10):
-    #     if random.randint(1, 100) < 25:
-    #         track1.add_rest(random.choice([0.25, 0.5, 1]))
-    #     else:
-    #         note += random.choice([-4, -3, -2, -1, 0, 1, 2, 3, 4])
-    #         track1.add_note(random.choice([0.25, 0.5, 1, 2]), note, velocity=100, sustain=0)
-    for i in range(1, 4):
-        note += random.choice([-4, -3, -2, -1, 0, 1, 2, 3, 4])
-        track1.add_note(random.choice([2]), note, velocity=100, sustain=0)
-    await track1.play(loop=True)
+    notes = generate_scale(major_scale, 2, 21)
+    note = 7
+    track1.add_note(1, notes[note])
+    for i in range(1, 8):
+        if random.randint(1, 100) < 50:
+            track1.add_rest(random.choice([0.25, 0.5, 1]))
+        else:
+            note += random.choice([-4, -3, -2, -1, 0, 1, 2, 3, 4])
+            track1.add_note(random.choice([0.25, 0.5, 1, 2]), notes[note], velocity=100, sustain=0)
+    # for i in range(1, 4):
+    #     note += random.choice([-4, -3, -2, -1, 0, 1, 2, 3, 4])
+    #     track1.add_note(random.choice([1]), notes[note], velocity=100, sustain=0)
+    await track1.play()
 
 
 asyncio.run(main())
