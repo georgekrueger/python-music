dc61e3d613d62886e5d2d83d6ab0b91b7eb1a592
georgekrueger <georgekrueger@gmail.com>
2022-01-15 11:34:46 -0600

    Update midisend.py

--- midisend.py
+++ midisend.py
@@ -5,9 +5,9 @@ import song
 
 major_scale = [2, 2, 1, 2, 2, 2, 1]
 
-def generate_scale(intervals, start_octave, length):
+def generate_scale(intervals, start_octave, num_octaves):
     notes = [start_octave * 12 + 12]
-    while len(notes) < length:
+    while len(notes) < num_octaves * len(intervals):
         interval = intervals[(len(notes) - 1) % len(intervals)]
         notes.append(notes[-1] + interval)
     return notes
@@ -39,11 +39,13 @@ async def main():
 
     beat = 0.5
     song1 = song.Song(beat * 8)
+    notes = generate_scale(major_scale, 1, 6)
     time = 0
+    n = 14
     for i in range(0, 8):
+        n += random.choice([-4, -3, -2, -1, 0, 1, 2, 3, 4])
         t = random.choice([beat/2, beat, beat*2])
-        # t = beat * 2
-        song1.add_event(time, song.Event(t, 36+i, random.randint(30,100), 0))
+        song1.add_event(time, song.Event(t, notes[n], random.randint(30,100), 0))
         time += t
 
     time = 0
