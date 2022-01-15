f89067c902582550ad6ef863bebe1782906b06ab
georgekrueger <georgekrueger@gmail.com>
2022-01-15 09:23:44 -0600

    Update midisend.py
    patting at the end of a track when adding to sequencer

--- midisend.py
+++ midisend.py
@@ -25,7 +25,7 @@ async def note(length, note_num=None, velocity=100, channel=0):
     outport.send(mido.Message('note_off', note=note_num, channel=channel))
 
 async def timer(t, callback):
-    print("start timer for %s" % (t))
+    #print("start timer for %s" % (t))
     await asyncio.sleep(t)
     callback()
 
@@ -53,12 +53,18 @@ class Sequencer:
                 self.events[t] = []
             self.events[t].append(event)
             t += event.length
+            if t >= track.length:
+                break
+        if t < track.length:
+            if t not in self.events:
+                self.events[t] = []
+            self.events[t].append(Event(track.length - t))
         self.times = list(self.events) # list of keys (times)
 
     def timer_callback(self):
         cur_time = self.times[self.cursor]
         events = self.events[cur_time]
-        print("sequencer timer. schedule %s events" % (len(events)))
+        #print("sequencer timer. schedule %s events" % (len(events)))
         for event in events:
             if event.note_num != None:
                 self.tasks.append(asyncio.create_task(note(event.length, event.note_num, event.velocity, event.channel)))
@@ -84,7 +90,8 @@ class Sequencer:
             await asyncio.wait(self.tasks)
 
 class Track:
-    def __init__(self, channel=0):
+    def __init__(self, length, channel=0):
+        self.length = length
         self.channel = channel
         self.events = []
 
@@ -97,9 +104,10 @@ class Track:
         self.events.append(Event(length))
 
 async def main():
-    track1 = Track(channel=0)
-    for i in range(1, 4):
+    track1 = Track(4, channel=0)
+    for i in range(1, 5):
         track1.add_note(0.5, 36+i, 100)
+
     # notes = generate_scale(major_scale, 2, 21)
     # n = 7
     # track1.add_note(1, notes[n])
@@ -111,8 +119,9 @@ async def main():
     #         # track1.add_note(random.choice([0.25, 0.5, 1, 2]), notes[n], velocity=100)
     #         track1.add_note(random.choice([0.5]), notes[n], velocity=100)
 
-    track2 = Track(channel=1)
-    for i in range(1, 4):
+    track2 = Track(4, channel=1)
+    for i in range(0, 8):
+        print(i)
         track2.add_note(0.5, 42, 100)
 
     sequencer = Sequencer()
